9b434658b5967d2bb05bf8b7ffb75fe3371dea5d
San624461 <sanjeda6244611@gmail.com>
2023-10-20 01:59:42 -0400

    environment variable added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 require('dotenv').config()
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
